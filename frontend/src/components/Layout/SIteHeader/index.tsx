9955f8bc792d3a636dcfccefabd7b5fb4c67c913
Jinnapat Janboonpeng <jinnapat.j@4-ti.com>
2021-06-13 13:42:49 +0700

    Update some images and styling

--- frontend/src/components/Layout/SIteHeader/index.tsx
+++ frontend/src/components/Layout/SIteHeader/index.tsx
@@ -9,8 +9,8 @@ const SiteHeader: FunctionComponent = () => {
   const history = useHistory()
   return (
     <div className="container mx-auto bg-primary rounded-b-full flex justify-between shadow-md px-6 py-2 h-12 relative">
-      <button className=" h-14 w-14" onClick={() => history.push('/')}>
-        <img className=" top-0" src="/img/logo.png" alt="logo" />
+      <button className="h-14 w-14 focus:outline-none" onClick={() => history.push('/')}>
+        <img className="top-0" src="/img/logo.png" alt="logo" />
       </button>
       <button
         className="text-sm font-bold bg-red-500 rounded-full p-2 focus:outline-none transition ease-out duration-500 hover:bg-orange-800 text-white"
--- frontend/src/orders/OrderConfirmation.tsx
+++ frontend/src/orders/OrderConfirmation.tsx
@@ -1,26 +1,37 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import Layout from '../components/Layout'
 import productSizeName from '../lib/productSizeName'
 import { OrderState, ProductSize, ProductState, UserState } from '../types'
+import { OrderRoutes } from './OrderRouters'
 
 const OrderConfirmaion: FunctionComponent = () => {
+  const history = useHistory()
   const { currentOrder } = useSelector((state: { orders: OrderState }) => state.orders)
   const { products } = useSelector((state: { products: ProductState }) => state.products)
   const { user } = useSelector((state: { users: UserState }) => state.users)
   const currentProduct = products.find((product) => product.id === currentOrder.product_id)
+
+  useEffect(() => {
+    if (!currentOrder.id || !products || !user.id) {
+      history.push(OrderRoutes.ORDER_INDEX)
+    }
+  }, [currentOrder.id, history, products, user.id])
+
   return (
     <Layout>
       <div className="w-full bg-yellow-100 p-4 my-2 rounded-2xl shadow-xl">
         <img
           src={`/img/${currentProduct?.type}.png`}
-          className="animate-spin-slow mx-auto w-3/5"
+          className="animate-spin-slow mx-auto w-3/5 md:w-2/5 lg:w-1/5"
           alt={currentProduct?.type}
         />
         <p className="text-3xl text-center font-bold">{`Congratulations ${user.first_name}!!`}</p>
         <p className="text-2xl text-center">{`Your ${currentOrder.number}x ${productSizeName(
           currentOrder.size as ProductSize
-        )} ${currentProduct?.type} are on the way to ${currentOrder.destination_address}!`}</p>
+        )} ${currentProduct?.type} are on the way to`}</p>
+        <p className="text-2xl text-center">{currentOrder.destination_address}!</p>
       </div>
     </Layout>
   )
