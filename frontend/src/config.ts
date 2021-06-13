ef6619fa75386e30c9f1e27c4a2dfff74c33146e
Jinnapat Janboonpeng <jinnapat.j@4-ti.com>
2021-06-13 13:55:38 +0700

    added env example

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -5,6 +5,6 @@ dotenv.config({ path: '.env' })
 export default {
   appName: 'Rikai',
   api: {
-    uri: process.env.REACT_APP_API_URI || 'http://192.168.1.54:4000',
+    uri: process.env.REACT_APP_API_URI || 'http://localhost:4000',
   },
 }
